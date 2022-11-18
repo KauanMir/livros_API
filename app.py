77a49b7adfe9e84ccc97d8fe945c6b02100c8766
Kauan Miranda <93612578+KauanMir@users.noreply.github.com>
2022-11-18 12:11:14 -0300

    Criando lista, consulta e consulta por id

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'titulo': 'O senhor dos Anéis - A Sociedade do Anel',
+        'autor': 'J.R.R Tolkien'
+    },
+    {
+        'id': 2,
+        'título': 'Harry Potter e a Pedra Filosofal',
+        'autor': 'J.K Howling'
+    },
+    {
+        'id': 3,
+        'título': 'James Clear',
+        'autor': 'Hábitos Atômicos'   
+    },
+]
+
+#consultar(todos)
+@app.route('/livros', methods=['GET'])
+def obter_livros():
+    return jsonify(livros)
+#contultar(id)
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livros_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+#editar
+#excluir
+
+app.run(port=5000, host='localhost', debug=True)
